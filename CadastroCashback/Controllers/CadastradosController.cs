a4206956191a9db114af21af49702c4a97730a76
SalesTg <tiagosalesg9@gmail.com>
2024-10-09 16:31:50 -0300

    Inicialização do projeto

---
+++ CadastroCashback/Controllers/CadastradosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroCashback.Controllers
+{
+    public class CadastradosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Deletar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
---
+++ CadastroCashback/Models/ContatoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CadastroCashback.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Boolean IsActive { get; set; }
+        public string Limite { get; set; }
+        public string Quant { get; set; }
+
+    }
+}
