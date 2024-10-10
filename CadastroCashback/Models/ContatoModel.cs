63c217473056fb333211776f75a0f6ee25a4f149
João Victor Sacramento Santana <89170370+JoaoSacramento99@users.noreply.github.com>
2024-10-09 22:19:15 -0300

    Update ContatoModel.cs

--- CadastroCashback/Models/ContatoModel.cs
+++ CadastroCashback/Models/ContatoModel.cs
@@ -1,12 +1,14 @@
-﻿namespace CadastroCashback.Models
+namespace CadastroCashback.Models
 {
     public class ContatoModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Celular { get; set; }
         public Boolean IsActive { get; set; }
-        public string Limite { get; set; }
-        public string Quant { get; set; }
+        public string? Limite { get; set; }
+        public string? Quant { get; set; }
 
     }
 }
